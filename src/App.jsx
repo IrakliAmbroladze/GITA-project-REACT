3b5b3b0e538f816334900b4c9c2dd68694036d54
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-02-26 14:49:55 +0400

    feat: add tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,16 @@ function App() {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
-    address: [],
+    address: null,
   });
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
+    const { name } = e.target;
+    const value = e.target.type === "file" ? e.target.files[0] : e.target.value;
     setFormData((prevData) => ({
       ...prevData,
       [name]: value,
     }));
-    // const value = e.target.type === "file" ? e.target.files[0] : e.target.value;
-    // setFormData({ ...formData, [e.target.id]: value });
   };
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -24,31 +23,44 @@ function App() {
 
   return (
     <div>
-      GITA REACT
-      <form onSubmit={handleSubmit}>
+      <div className="text-3xl p-5">GITA REACT</div>
+      <form onSubmit={handleSubmit} className="flex flex-col p-5">
         <label htmlFor="name">name: </label>
         <input
           type="text"
+          id="name"
           name="name"
           value={formData.name}
           onChange={handleChange}
+          className="border-2 border-gray-300 rounded-md"
         />
         <br />
         <label htmlFor="email">email: </label>
         <input
           type="text"
+          id="email"
           name="email"
           value={formData.email}
           onChange={handleChange}
+          className="border-2 border-gray-300 rounded-md"
         />
         <br />
-        <label htmlFor="address">address: </label>
+        <label htmlFor="address">upoad proof of address: </label>
         <input
           type="file"
+          id="address"
           name="address"
-          value={formData.address}
           onChange={handleChange}
         />
+        {formData.address && (
+          <div className="flex justify-center">
+            <img
+              src={URL.createObjectURL(formData.address)}
+              alt="Preview"
+              className="mt-2 h-20 rounded w-40 h-40 "
+            />
+          </div>
+        )}
         <br />
         <button type="submit">submit</button>
       </form>

ef7c201e1e8e84966a06d61c230d45c3328efe7f
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-03-20 16:50:57 +0400

    feat(sort): sort data alph. by name

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import "./App.css";
 function App() {
   const [employees, setEmployees] = useState([]);
   const [listOpen, setListOpen] = useState(false);
+  const [sortOpen, setSortOpen] = useState(false);
   useEffect(() => {
     const loadEmployees = async () => {
       try {
@@ -19,21 +20,41 @@ function App() {
   const handleListClick = () => {
     setListOpen(() => !listOpen);
   };
+  const handleSortClick = () => {
+    setSortOpen(() => !sortOpen);
+  };
 
   return (
     <div>
       <div className="text-3xl p-5">GITA REACT</div>
-      <div>
-        <button onClick={handleListClick}>List</button>
-        {listOpen && (
-          <ul>
-            {employees.map((employee, index) => (
-              <li key={index}>
-                {employee.name} - {employee.department} - {employee.role}
-              </li>
-            ))}
-          </ul>
-        )}
+      <div className="flex">
+        <div>
+          <button onClick={handleListClick}>List</button>
+          {listOpen && (
+            <ul>
+              {employees.map((employee, index) => (
+                <li key={index}>
+                  {employee.name} - {employee.department} - {employee.role}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+        <div>
+          <button onClick={handleSortClick}>Sort</button>
+          {sortOpen && (
+            <ul>
+              {employees
+                .slice()
+                .sort((a, b) => a.name.localeCompare(b.name))
+                .map((employee, index) => (
+                  <li key={index}>
+                    {employee.name} - {employee.department} - {employee.role}
+                  </li>
+                ))}
+            </ul>
+          )}
+        </div>
       </div>
     </div>
   );
